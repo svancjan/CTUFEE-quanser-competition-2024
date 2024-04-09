fcff52f363cc4a1795fec638bb4a41bf5b7213b7
Ondřej Mikoláš <mikolond@fel.cvut.cz>
2024-04-09 14:50:54 +0200

    Visioin node for ros added

---
+++ vision_node.py
@@ -0,0 +1,63 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import ByteMultiArray
+import time
+import sys
+import markers_detection as md
+import pickle
+
+def serialize_message(message):
+    try:
+        message = pickle.dumps(message)
+        return message
+    except pickle.PicklingError as e:
+        print(f"Error occurred while serializing message: {e}")
+        return None
+
+def deserialize_message(message):
+    try:
+        message = pickle.loads(message)
+        return message
+    except pickle.UnpicklingError as e:
+        print(f"Error occurred while deserializing message: {e}")
+        return None
+
+class vision_node(Node):
+    def __init__(self):
+        super().__init__('vision_node')
+        
+        qos_profile = rclpy.qos.QoSProfile(
+            reliability=rclpy.qos.QoSReliabilityPolicy.BEST_EFFORT,
+            history=rclpy.qos.QoSHistoryPolicy.KEEP_LAST,
+            depth=1
+        )
+        self.vision_publisher = self.create_publisher(ByteMultiArray, 'VisionData', qos_profile=qos_profile)
+
+        self.subscription2 = self.create_subscription(
+            ByteMultiArray,
+            'CameraRGB',
+            self.processData,
+            qos_profile = qos_profile,
+            raw=True
+        )
+
+    def processData(self, message):
+        # needs to get image[0] becaise it returns tunple (image, timestamp)
+        image = deserialize_message(message)[0]
+        print("Received msg type: ", type(image))
+
+        # if data are image do this:
+
+        bb_stop = md.detect_stop_sign(image)
+        bb_traffic = md.detect_traffic_lights(image)
+
+        # serialize data and publish them
+        msg = serialize_message((bb_stop, bb_traffic))
+        self.vision_publisher.publish(msg)
+        
+
+
+
+
+
+
